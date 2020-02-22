3e6c635afb8be4dbbc20d6b532217b3aa5e16577
rvpanoz <rvpanoz@gmail.com>
2020-02-22 19:40:42 +0200

    feature(setup): init project, add server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+/**
+ * Node.js HTTP server
+ */
+
+const http = require("http");
+
+/**
+ * Request handler
+ * @param {*} req
+ * @param {*} res
+ */
+const requestListener = function(req, res) {
+  res.writeHead(200);
+  res.end("Hello, Node!");
+};
+
+// create HTTP server
+const server = http.createServer(requestListener);
+
+// start the server
+server.listen(8080);
