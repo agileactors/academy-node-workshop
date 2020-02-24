baf56d1a6e508f105e2f20456a173f59d1d17fd8
alexalikiotis <alexalikiotis5@gmail.com>
2020-02-24 20:59:27 +0200

    adding http router example

--- server.js
+++ server.js
@@ -4,23 +4,36 @@
 
 const http = require('http');
 const logger = require('./libraries/logger'); // task: move logger to module
+const Router = require('./libraries/router');
 
 const PORT = process.argv[2] || 8080; // task: use process to get arguments from cli
 
-/**
- * Request handler
- * @param {*} req
- * @param {*} res
- */
-const requestListener = (req, res) => {
-  // task: implement the handler
-  res.setHeader('Content-Type', 'text/html');
-  res.writeHead(200);
-  res.end('Hello, Node!');
-};
+// initialize router
+const router = Router();
+
+// watcher middleware - TODO: move middleware to another folder
+router.use(async (ctx, next) => {
+  const { request } = ctx;
+
+  const start = Date.now();
+  await next();
+  const duration = Date.now() - start;
+
+  logger.log(`${request.method} ${request.url} - ${duration}ms`);
+});
+
+// index route
+router.create('GET', '/', ({ response }) => {
+  response.setHeader('Content-Type', 'text/html');
+  response.writeHead(200);
+  response.end('Hello, Node!');
+});
 
 // create HTTP server
-const server = http.createServer(requestListener);
+const server = http.createServer((request, response) => {
+  const ctx = { request, response };
+  router.run(ctx);
+});
 
 // start the server
 server.listen(PORT, () => {
