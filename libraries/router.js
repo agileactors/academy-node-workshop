2ecd206171df53925dcb569d135f9106f31b2f9c
alexalikiotis <alexalikiotis5@gmail.com>
2020-02-25 16:51:28 +0200

    add: updating the http router

--- libraries/router.js
+++ libraries/router.js
@@ -9,12 +9,18 @@ const Router = () => {
   const middleware = [];
 
   // register new route
-  const create = (method, path, handler) => {
+  const createRoute = (method, path, handler) => {
     routes.push({ method, path, handler });
   };
 
+  // expose http methods as functions
+  const get = (path, handler) => createRoute('GET', path, handler);
+  const post = (path, handler) => createRoute('POST', path, handler);
+  const put = (path, handler) => createRoute('PUT', path, handler);
+  const del = (path, handler) => createRoute('DELETE', path, handler);
+
   // find the appropriate route for the request
-  const routesMiddleware = ctx => {
+  const routesMiddleware = (ctx, next) => {
     const { request } = ctx;
     const { pathname } = url.parse(request.url);
 
@@ -22,7 +28,11 @@ const Router = () => {
       r => r.method === request.method && r.path === pathname
     );
 
-    if (route) route.handler(ctx);
+    if (route) {
+      route.handler(ctx);
+      return;
+    }
+    next();
   };
 
   // register new middleware
@@ -30,21 +40,21 @@ const Router = () => {
 
   // start the middleware chain
   const run = ctx => {
-    // add routes as the last middleware
-    middleware.push(routesMiddleware);
-
     const dispatch = async i => {
       const currentMiddleware = middleware[i];
       if (currentMiddleware) currentMiddleware(ctx, () => dispatch(i + 1));
     };
-
     dispatch(0);
   };
 
   return {
+    get,
+    post,
+    put,
+    del,
     use,
-    create,
     run,
+    routesMiddleware,
   };
 };
 
