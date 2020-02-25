e76ffa52fc125c9b617f6bc4ff4f1ffe5005505c
rvpanoz <rvpanoz@gmail.com>
2020-02-25 23:38:16 +0200

    fix PR comments

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
  * Applications configuration file
  */
 
-const { DB_NAME, DB_USER, DB_PASSWORD } = process.env;
-const URI = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0-diifo.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
+const { DB_HOST, DB_USER, DB_PASSWORD } = process.env;
+const URI = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0-diifo.mongodb.net/${DB_HOST}?retryWrites=true&w=majority`;
 
 const config = {
   URI,
