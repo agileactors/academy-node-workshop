526698ff02e5efc96629ca171a9f06ad43a28b81
rvpanoz <rvpanoz@gmail.com>
2020-02-26 00:21:28 +0200

    fix: DB_HOST env value

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
  */
 
 const { DB_HOST, DB_USER, DB_PASSWORD } = process.env;
-const URI = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0-diifo.mongodb.net/${DB_HOST}?retryWrites=true&w=majority`;
+const URI = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}?retryWrites=true&w=majority`;
 
 const config = {
   URI,
