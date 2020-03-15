67ffe8e57f9fd61a020adf48e3be0dc2cd3d208a
Panagiotis.Arvanitis <Panagiotis.Arvanitis@gfk.com>
2020-03-16 00:22:12 +0200

    wip: initial tasks

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log(process.argv);
+/*
+ * TODO: 
+ * 
+ * - Write to the console "Hello Node.js server!"
+ * 
+ */
+
+ // implementation..
+
+ /*
+ * TODO: 
+ * 
+ * - Read argument NODE_ENV and PORT from the CLI
+ * 
+ */
+
+ // implementation..
+
