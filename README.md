ca5ba767a9a0e6ebdd956532418abada70493aa3
Panagiotis.Arvanitis <Panagiotis.Arvanitis@gfk.com>
2020-03-21 14:32:28 +0200

    feat: add solution

--- README.md
+++ README.md
@@ -4,15 +4,11 @@ Node.js Workshop
 
 ## Branch description
 
-In this branch we will use the File System(fs) module to create a .env file
-for our application.
+In this branch we will introduce some of node.js global objects and the path module
 
 ## Branch Tasks
 
-1. Provide the checkEnv implementation which checks if the .env file exists in the root folder.
-2. Provide the readEnv implementation which reads the contents of .env if exists
-3. Provide the createEnv implmentation which creates the .env file
-
 ## Covers
 
-- File System module (fs)
+- Path module (path)
+- Node.js global objects
--- init.js
+++ init.js
@@ -16,49 +16,48 @@ const getEnvContent = () => {
   return envValues.join('\n');
 };
 
-/**
- * Task 3: createEnv implementation
- *
- * Use the getEnvContent to get the .env content values
- * use the fs.writeFile to write the content values to a file with name .env in the root folder.
- * Print 
- 
- * Hint: use utf8 encoding
- */
-
 const createEnv = () => {
   try {
     const data = getEnvContent();
 
-    /** provide your implementation * */
+    fs.writeFile('.env', data, 'utf8', err => {
+      if (err) {
+        throw err;
+      }
+
+      console.log('Finished .env configuration');
+    });
   } catch (error) {
     console.error(error);
   }
 };
 
-/**
- * Task 2: readEnv implementation
- *
- * Use the fs.readFile api to read the contents of the .env file
- * and print the contents of the file to the stdout.
- * If an error occurs throw an exception.
- *
- * Hint: use utf8 encoding
- */
+const readEnv = () => {
+  try {
+    fs.readFile('.env', 'utf8', (err, data) => {
+      if (err) {
+        throw err;
+      }
 
-const readEnv = () => {};
+      console.log(`.env configuration: \n${data}\n`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-/**
- * Task 1: checkEnv implementation
- *
- * Use the fs.open api to check if the file .env exists in the project root folder.
- * If not exists print the message '"Starting configuration.." to the console and call createEnv function after 2 seconds and return'
- * If exists call the readEnv function.
- *
- * Hint: use appropriate flag for read and writing
- */
+const checkEnv = () => {
+  const envPath = '.env';
 
-const checkEnv = () => {};
+  fs.open(envPath, 'r+', err => {
+    if (err) {
+      console.log('Starting configuration..');
+      return createEnv();
+    }
+
+    readEnv();
+  });
+};
 
 console.log('starting application..\n');
 setTimeout(checkEnv, 1500);
