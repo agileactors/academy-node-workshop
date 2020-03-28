d8a6ecb535cf6cf1987d6f4c68d9b13999aba93f
rvpanoz <rvpanoz@gmail.com>
2020-03-28 17:39:32 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,5 @@
 /**
  * Node.js HTTP server
  */
-const http = require('http');
 
-/**
- * Task 1: create a simple http server
- *
- * Use the http module to create an http server that
- * responds with the text "Hello World!".
- 
- * Hint: use the PORT env variable as the server's listening port
- 
- * Task 2: Create a simple http router
- *
- * Using the url module to get the 'pathname' from the request URL
- * and create the following endpoints:
- *
- *  1. '/' => <h1>Index Page</h1>
- *  2. '/about' => <h1>About Page</h1>
- *
- * Hint: The response here is in html format so you need to 
- * set the appropriate headers before sending the response.
- */
+const http = require('http');
