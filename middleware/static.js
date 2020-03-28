50f54767b6a2823bc0a2acdefc05e40698a11e6f
Panagiotis.Arvanitis <Panagiotis.Arvanitis@gfk.com>
2020-03-28 11:12:22 +0200

    feat: support mimes, add font-awesome

--- middleware/static.js
+++ middleware/static.js
@@ -17,6 +17,32 @@ const rootDir = process.cwd();
 const staticDir = path.join(rootDir, 'static');
 
 const middleware = async ({ request, response }, next) => {
+  let filePath = `.${request.url}`;
+  console.log(filePath);
+  if (filePath === './') {
+    filePath = './index.html';
+  }
+
+  const extname = String(path.extname(filePath)).toLowerCase();
+  const mimeTypes = {
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+    '.json': 'application/json',
+    '.png': 'image/png',
+    '.jpg': 'image/jpg',
+    '.gif': 'image/gif',
+    '.svg': 'image/svg+xml',
+    '.wav': 'audio/wav',
+    '.mp4': 'video/mp4',
+    '.woff': 'application/font-woff',
+    '.ttf': 'application/font-ttf',
+    '.eot': 'application/vnd.ms-fontobject',
+    '.otf': 'application/font-otf',
+  };
+
+  const contentType = mimeTypes[extname] || 'application/octet-stream';
+
   try {
     const staticPath = path.join(staticDir, request.url);
     const stats = await stat(staticPath);
@@ -30,7 +56,7 @@ const middleware = async ({ request, response }, next) => {
     // read file's content
     const content = await readFile(staticPath);
 
-    response.writeHead(200, { 'Content-Type': 'text/css' });
+    response.writeHead(200, { 'Content-Type': contentType });
     response.end(content);
   } catch (err) {
     next();
