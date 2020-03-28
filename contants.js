8ffbe8b214865dee4b99b651948f31e977403d90
Panagiotis.Arvanitis <Panagiotis.Arvanitis@gfk.com>
2020-03-28 11:18:20 +0200

    feat: add constants file

---
+++ contants.js
@@ -0,0 +1,20 @@
+const constants = {
+  MIMETYPES: {
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+    '.json': 'application/json',
+    '.png': 'image/png',
+    '.jpg': 'image/jpg',
+    '.gif': 'image/gif',
+    '.svg': 'image/svg+xml',
+    '.wav': 'audio/wav',
+    '.mp4': 'video/mp4',
+    '.woff': 'application/font-woff',
+    '.ttf': 'application/font-ttf',
+    '.eot': 'application/vnd.ms-fontobject',
+    '.otf': 'application/font-otf',
+  },
+};
+
+module.exports = constants;
--- middleware/static.js
+++ middleware/static.js
@@ -5,6 +5,8 @@
 const fs = require('fs');
 const path = require('path');
 const { promisify } = require('util');
+const logger = require('../libraries/logger');
+const { MIMETYPES } = require('../contants');
 
 // promisify readFile utility
 const stat = promisify(fs.stat);
@@ -17,39 +19,17 @@ const rootDir = process.cwd();
 const staticDir = path.join(rootDir, 'static');
 
 const middleware = async ({ request, response }, next) => {
-  let filePath = `.${request.url}`;
-  console.log(filePath);
-  if (filePath === './') {
-    filePath = './index.html';
-  }
-
-  const extname = String(path.extname(filePath)).toLowerCase();
-  const mimeTypes = {
-    '.html': 'text/html',
-    '.js': 'text/javascript',
-    '.css': 'text/css',
-    '.json': 'application/json',
-    '.png': 'image/png',
-    '.jpg': 'image/jpg',
-    '.gif': 'image/gif',
-    '.svg': 'image/svg+xml',
-    '.wav': 'audio/wav',
-    '.mp4': 'video/mp4',
-    '.woff': 'application/font-woff',
-    '.ttf': 'application/font-ttf',
-    '.eot': 'application/vnd.ms-fontobject',
-    '.otf': 'application/font-otf',
-  };
-
-  const contentType = mimeTypes[extname] || 'application/octet-stream';
-
   try {
+    const { url: filePath } = request;
+    const extname = String(path.extname(filePath)).toLowerCase();
+    const contentType = MIMETYPES[extname] || 'application/octet-stream';
     const staticPath = path.join(staticDir, request.url);
     const stats = await stat(staticPath);
 
-    // if path is not file continue the chain
+    // if path is not a file continue the chain
     if (!stats.isFile()) {
       next();
+
       return;
     }
 
@@ -59,6 +39,7 @@ const middleware = async ({ request, response }, next) => {
     response.writeHead(200, { 'Content-Type': contentType });
     response.end(content);
   } catch (err) {
+    logger.log(err);
     next();
   }
 };
