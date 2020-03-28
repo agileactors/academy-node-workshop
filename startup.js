2fd2c8e86176ba54ea1e79ecd0fa0866c7e96cfa
alexalikiotis <alexalikiotis5@gmail.com>
2020-03-28 20:34:43 +0200

    Updating startup.js

--- startup.js
+++ startup.js
@@ -1,1 +1,20 @@
-require('./server');
+const server = require('./server');
+const logger = require('./libraries/logger');
+
+// PORT env variable
+const PORT = process.env.PORT || 8001;
+
+// start the server
+server.listen(PORT, () => {
+  logger.log(`Server listening on port ${PORT}`);
+});
+
+process.on('uncaughtException', error => {
+  logger.log(error);
+  process.exit(0);
+});
+
+process.on('unhandledRejection', error => {
+  logger.log(error);
+  process.exit(0);
+});
