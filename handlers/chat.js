aa35f011bf1ea40c5ad87d1b9251776af5204593
rvpanoz <rvpanoz@gmail.com>
2020-03-29 21:18:34 +0300

    Update chat.js

--- handlers/chat.js
+++ handlers/chat.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const path = require('path');
 const shortid = require('shortid');
-
 const logger = require('../libraries/logger');
 
 const rootDir = process.cwd();
@@ -10,7 +9,7 @@ const rootDir = process.cwd();
  *
  * Task 1: Create a handler that responds with html
  *
- * Use the fs module to read the chat.html file as a stream an send it
+ * Use the fs module to read the chat.html file as a stream and send it
  * as the response.
  *
  * Hint: Use the pipe operator between the fs and response streams
@@ -28,7 +27,7 @@ const get = ({ response }) => {
  * Use the global JSON object to create a JSON response containing a new
  * chat username. e.x { username: 'user_fh23FFGJ' }
  *
- * Hint 1: Use the the async stringify method from the JSON global
+ * Hint 1: Use the async stringify method from the JSON global
  * Hint 2: Use 'shortid.generate()' to create a random ID for the username
  *
  */
