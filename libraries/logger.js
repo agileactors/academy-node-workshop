11f4c95086fdb7cdc5088c4cd17f82b6fae7298a
Panagiotis.Arvanitis <Panagiotis.Arvanitis@gfk.com>
2020-03-29 22:42:08 +0300

    docs: update

--- libraries/logger.js
+++ libraries/logger.js
@@ -1,11 +1,11 @@
 /**
  * Task 1
  *
- * Implement the logger log and debug methods. The logger must use asynchronous apis
- * and the debug.log file must be placed in the root folder.
+ * Implement the logger log function.
+ * The function must use asynchronous apis and the debug.log file must be placed in the root folder.
  *
  * 1. It writes debug statements in the console
- * 2. it writes debug statements in debug.log file
+ * 2. it writes debug statements in the debug.log file
  * 3. It writes the timestamp at the beggining of each line
  * 3. Can handle multiple values and types such as <string | object | array> (bonus)
  *
@@ -14,13 +14,15 @@
  *
  * Result:
  * [timestamp, message].
- * e.g ["hours:minutes:seconds","some info" | { error: 'some error } | ['some item1', 'some item2']]
+ * e.g ["day/month/year hours:minutes","some info" | { error: 'some error } | ['some item1', 'some item2']]
  */
 
 const logger = {
   logToFile: true,
   logToConsole: true,
-  log(...args) {},
+  log(...args) {
+    /** TODO: provide implementation */
+  },
 };
 
 module.exports = logger;
