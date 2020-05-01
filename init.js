698fc289f1e09976acefa580574df5323bc4de60
Panagiotis.Arvanitis <Panagiotis.Arvanitis@gfk.com>
2020-05-01 14:25:11 +0300

    tasks: update

--- init.js
+++ init.js
@@ -3,24 +3,13 @@ const path = require('path');
 const { platform, arch, release, totalmem, freemem } = require('os');
 
 const logger = require('./libraries/logger');
-const { ENVVALUES } = require('./constants');
+const { nwsGetEnvContent } = require('./constants');
 
 const cwd = process.cwd();
 const ENV_PATH = path.join(cwd, '.env');
 const LOGS_DIR = path.join(cwd, 'logs');
 const args = process.argv.slice(2, process.argv.length);
 const bypassCheck = args.some(arg => arg === 'BYPASS');
-
-// Do not edit
-const nwsGetEnvContent = () => {
-  const envValues = ENVVALUES.reduce((acc, envValue) => {
-    const { name, value } = envValue;
-
-    return [...acc, `${name}=${value}`];
-  }, []);
-
-  return envValues.join('\n');
-};
 
 const createEnv = () => {
   const envFileContent = nwsGetEnvContent();
