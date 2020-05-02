a1ac4c5f651279f7e45e83b8fff00a7981d9c247
Panagiotis.Arvanitis <Panagiotis.Arvanitis@gfk.com>
2020-05-02 12:36:55 +0300

    tasks: update

--- server.js
+++ server.js
@@ -16,10 +16,12 @@ const logger = require('./libraries/logger');
  *
  * Using the request.url property create the following endpoints:
  *
- *  1. '/home' => <h1>Home Page</h1>
- *  2. '/chat' => <h1>Chat Page</h1>
- *
- * Hint: The response should be in html format so you need to
+ *  1. '/' => serves views/home.html
+ *  2. '/chat' => serves views/chat.html
+ *  3. if the requested route not found return a http 404 with message 'Not found'
+ * 
+ * 
+ * Hint: The response should be in html or text format so you need to
  * set the appropriate headers before sending the response.
  *
  
