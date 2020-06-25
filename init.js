7af47933739563a464d6ba574925fd14954afdb7
Panagiotis.Arvanitis <Panagiotis.Arvanitis@gfk.com>
2020-06-25 22:24:38 +0300

    fix: use fn instead of  arrow fn

--- init.js
+++ init.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
-const path = require('path');
 const { platform, arch, release, totalmem, freemem } = require('os');
-
+const path = require('path');
 const logger = require('./libraries/logger');
 const { nwsGetEnvContent } = require('./libraries/utilities');
 
@@ -9,34 +8,14 @@ const cwd = process.cwd();
 const ENV_PATH = path.join(cwd, '.env');
 const LOGS_DIR = path.join(cwd, 'logs');
 const args = process.argv.slice(2, process.argv.length);
-const bypassCheck = args.some(arg => arg === '--bypass');
+const bypassCheck = args.some(arg => arg === '--bypass' || '-b');
 
-const createEnv = () => {
-  const envFileContent = nwsGetEnvContent();
-
-  fs.writeFile(ENV_PATH, envFileContent, 'utf8', err => {
-    if (err) {
-      throw err;
-    }
-
-    console.log('Finished .env configuration.');
-  });
-};
-
-const readEnv = () => {
-  try {
-    const data = fs.readFileSync(ENV_PATH, 'utf8');
-
-    console.log(`Found configuration: \n${data}\n`);
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-const checkEnv = () => {
-  console.log(`Your Operating System: ${platform()} ${arch()} ${release()}`);
+function checkEnv() {
   console.log(
-    `${((freemem() / totalmem()) * 100).toFixed(2)} % of your RAM is free.\n`
+    `Your Operating System: ${platform()} ${arch()} ${release()}\n${(
+      (freemem() / totalmem()) *
+      100
+    ).toFixed(2)} % of your RAM is free.`
   );
 
   try {
@@ -51,22 +30,43 @@ const checkEnv = () => {
     fs.open(ENV_PATH, 'wx', err => {
       if (err) {
         if (err.code === 'EEXIST') {
-          return readEnv();
+          return readEnv(); // eslint-disable-line
         }
 
         throw err;
       }
 
-      createEnv();
+      createEnv(); // eslint-disable-line
     });
   } else {
     console.log('Bypassing configuration..');
   }
-};
+}
+
+function createEnv() {
+  const envFileContent = nwsGetEnvContent();
+
+  fs.writeFile(ENV_PATH, envFileContent, 'utf8', err => {
+    if (err) {
+      throw err;
+    }
+
+    console.log('Finished .env configuration.');
+  });
+}
+
+function readEnv() {
+  try {
+    const data = fs.readFileSync(ENV_PATH, 'utf8');
+
+    console.log(`Found configuration: \n${data}\n`);
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 process.on('uncaughtException', err => {
-  console.log(err.stack);
-  logger.log(`pid ${process.pid}: ${err.message}`);
+  logger.log(`pid ${process.pid}\n${err.message}`);
   process.exit(0);
 });
 
