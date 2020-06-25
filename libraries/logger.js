a6f2cc6861c3500bb7991da7096608d21a369d48
Panagiotis.Arvanitis <Panagiotis.Arvanitis@gfk.com>
2020-06-25 23:50:25 +0300

    docs: update

--- libraries/logger.js
+++ libraries/logger.js
@@ -6,7 +6,7 @@ const { nwsFormatDate, nwsConcatValues } = require('./utilities');
  * Implement the logger log function.
  *
  * 1. It should writes debug statements to the console
- * 2. it should writes debug statements to the debug.log file located in the logs directory.
+ * 2. it should writes debug statements to the debug.log file placed in the logs directory.
  * 3. It should writes the timestamp at the beggining of each line
  * 4. Can handle multiple values and types such as <string | object | array> (bonus)
  *
