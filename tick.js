51a5600863cd0596b142c7d1b106d208a997b3a1
Alex Alikiotis <alexalikiotis5@gmail.com>
2023-03-07 21:35:22 +0200

    Updating dependencies

--- tick.js
+++
@@ -1,9 +0,0 @@
-const racer = () => {
-  setTimeout(() => console.log('timeout1'), 1000);
-  // setImmediate(() => console.log('immediate'));
-  console.log('console');
-  process.nextTick(() => console.log('nextTick'));
-  setTimeout(() => console.log('timeout0'), 0);
-};
-
-racer();
